7fb660ef5bf8f7e6b48da74deb3333da520ec273
KevinArchive1 <haroldvelena27@gmail.com>
2025-05-04 07:41:30 +0800

    new

--- routers/src/App.js
+++ routers/src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
-import NavBar from './componets/NavBar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contacts from './pages/Contacts';
-import Footer from './componets/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/routers-in-react">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contacts" element={<Contacts />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contacts' element={<Contacts />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </Router>
